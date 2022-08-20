d01142d5424d50440f680eacde790df7ff64ec87
spyhere <gekav3@mail.ru>
2022-08-20 23:58:31 +0300

    Move matches fixture to fixtures directory

---
+++ tests/fixtures/matches.ts
@@ -0,0 +1,11 @@
+import { Match } from "../../components/scoreboard"
+
+const matches: Match[] = [
+  [["Mexico", "Canada"], [0, 5]],
+  [["Spain", "Brazil"], [10, 2]],
+  [["Germany", "France"], [2, 2]],
+  [["Uruguay", "Italy"], [6, 6]],
+  [["Argentina", "Australia"], [3, 1]],
+]
+
+export default matches
--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -1,14 +1,8 @@
-import Scoreboard, { Match } from "../components/scoreboard"
+import Scoreboard from "../components/scoreboard"
+import initialMatches from "./fixtures/matches"
 
 describe("Scoreboard basic operations", () => {
   let board
-  const initialMatches: Match[] = [
-    [["Mexico", "Canada"], [0, 5]],
-    [["Spain", "Brazil"], [10, 2]],
-    [["Germany", "France"], [2, 2]],
-    [["Uruguay", "Italy"], [6, 6]],
-    [["Argentina", "Australia"], [3, 1]],
-  ]
 
   beforeEach(() => {
     board = new Scoreboard(initialMatches)
