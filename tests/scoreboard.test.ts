05b34617ff5e1d0d636179d76b163539d1e1d4ba
spyhere <gekav3@mail.ru>
2022-08-20 19:59:44 +0300

    Initialize main test cases

---
+++ tests/scoreboard.test.ts
@@ -0,0 +1,12 @@
+
+describe("Scoreboard basic operations", () => {
+
+  it.todo("should start new game")
+
+  it.todo("finish game currently in progress")
+
+  it.todo("update the score")
+
+  it.todo("get a summary of games in progress ordered by their total score")
+
+})
