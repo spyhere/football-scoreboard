705bea8d85d9b5c151ec869c01cb25099324e41c
spyhere <gekav3@mail.ru>
2022-08-20 20:21:32 +0300

    Write "new game" test

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -1,7 +1,28 @@
+class ScoreBoard {
+  constructor(matches) {
+  }
+}
 
 describe("Scoreboard basic operations", () => {
+  let board
+  const initialMatches = [
+    [["Mexico", "Canada"], [0, 5]],
+    [["Spain", "Brazil"], [10, 2]],
+    [["Germany", "France"], [2, 2]],
+    [["Uruguay", "Italy"], [6, 6]],
+    [["Argentina", "Australia"], [3, 1]],
+  ]
 
-  it.todo("should start new game")
+  beforeEach(() => {
+    board = new ScoreBoard(initialMatches)
+  })
+
+  it("should start new game", () => {
+    const [homeTeam, guestTeam] = ["Team A", "Team B"]
+    const newMatch = board.addMatch([homeTeam, guestTeam])
+    expect(newMatch).toBe(`${homeTeam} 0 - ${guestTeam} 0`)
+    expect(board.activeMatchesCount).toBe(initialMatches.length + 1)
+  })
 
   it.todo("finish game currently in progress")
 
