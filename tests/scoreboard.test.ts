343d346afdb3ab876340d1b15c97b2688342b5ca
spyhere <gekav3@mail.ru>
2022-08-20 22:23:43 +0300

    Add Match type to tests

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -1,11 +1,8 @@
-class ScoreBoard {
-  constructor(matches) {
-  }
-}
+import Scoreboard, { Match } from "../components/scoreboard"
 
 describe("Scoreboard basic operations", () => {
   let board
-  const initialMatches = [
+  const initialMatches: Match[] = [
     [["Mexico", "Canada"], [0, 5]],
     [["Spain", "Brazil"], [10, 2]],
     [["Germany", "France"], [2, 2]],
@@ -14,7 +11,7 @@ describe("Scoreboard basic operations", () => {
   ]
 
   beforeEach(() => {
-    board = new ScoreBoard(initialMatches)
+    board = new Scoreboard(initialMatches)
   })
 
   it("should start new game", () => {
