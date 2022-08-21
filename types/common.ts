7ca6c340c17e7ac0327208e091412dc6736f481c
spyhere <gekav3@mail.ru>
2022-08-21 23:52:33 +0300

    Add naming aliases for team and score types

--- types/common.ts
+++ types/common.ts
@@ -1,3 +1,8 @@
-export type Teams = [string, string]
-export type Score = [number, number]
+type HomeTeamName = string
+type AwayTeamName = string
+export type Teams = [HomeTeamName, AwayTeamName]
+
+type HomeTeamScore = number
+type AwayTeamScore = number
+export type Score = [HomeTeamScore, AwayTeamScore]
 export type Match = [Teams, Score]
